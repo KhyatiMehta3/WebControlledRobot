847717b5f3ebd2e1a26c164e2a9e2428c4bcc080
KhyatiMehta3 <khyatimehta69@gmail.com>
2018-04-04 20:15:53 +0530

    Update motors.py

--- motors.py
+++ motors.py
@@ -1,85 +1,61 @@
-#######
-# Author: James Poole
-# Date: 23 April 2016
-# jgaple@gmail.com
-#
-# motors.py
-#######
-
 import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(03, GPIO.OUT) 
+GPIO.setup(05, GPIO.OUT)     
+GPIO.setup(07, GPIO.OUT) 
+GPIO.setup(11, GPIO.OUT) 
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+pwm1 = GPIO.PWM(13,100)
+pwm2 = GPIO.PWM(15,100)
 
-Motor1A = 16 #(GPIO 23 - Pin 16)
-Motor1B = 18 #(GPIO 24 - Pin 18)
-Motor1Enable = 22 #(GPIO 25 - Pin 22)
-
-Motor2A = 21 #(GPIO 9 - Pin 21)
-Motor2B = 19 #(GPIO 10 - Pin 19)
-Motor2Enable = 23 #(GPIO 11 - Pin 23)
-
-
-#Set up all as outputs
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1Enable,GPIO.OUT)
-GPIO.setup(Motor2A,GPIO.OUT)
-GPIO.setup(Motor2B,GPIO.OUT)
-GPIO.setup(Motor2Enable,GPIO.OUT)
-
-
-def forward():
-	print("Going Forwards")
-	GPIO.output(Motor1A,GPIO.HIGH)
-	GPIO.output(Motor1B,GPIO.LOW)
-	GPIO.output(Motor1Enable,GPIO.HIGH) #Enable Motor 1 High
-	GPIO.output(Motor2A,GPIO.HIGH)
-	GPIO.output(Motor2B,GPIO.LOW)
-	GPIO.output(Motor2Enable,GPIO.HIGH) #Enable Motor 2 High
-
-	sleep(2)
-
-
-
-def backward():
-	print("Going Backwards")
-	GPIO.output(Motor1A,GPIO.LOW)
-	GPIO.output(Motor1B,GPIO.HIGH)
-	GPIO.output(Motor1Enable,GPIO.HIGH)
-	GPIO.output(Motor2A,GPIO.LOW)
-	GPIO.output(Motor2B,GPIO.HIGH)
-	GPIO.output(Motor2Enable,GPIO.HIGH)
-
-	sleep(2)
-
-def turnRight():
-	print("Going Right")
-	GPIO.output(Motor1A,GPIO.HIGH)
-	GPIO.output(Motor1B,GPIO.LOW)
-	GPIO.output(Motor1Enable,GPIO.HIGH)
-	GPIO.output(Motor2A,GPIO.LOW)
-	GPIO.output(Motor2B,GPIO.LOW)
-	GPIO.output(Motor2Enable,GPIO.LOW)
-
-	sleep(2)
-
-def turnLeft():
-	print("Going Left")
-	GPIO.output(Motor1A,GPIO.LOW)
-	GPIO.output(Motor1B,GPIO.LOW)
-	GPIO.output(Motor1Enable,GPIO.LOW)
-	GPIO.output(Motor2A,GPIO.HIGH)
-	GPIO.output(Motor2B,GPIO.LOW)
-	GPIO.output(Motor2Enable,GPIO.HIGH)
-
-	sleep(2)
-
-def stop():
-	print("Stopping")
-	GPIO.output(Motor1A,GPIO.LOW)
-	GPIO.output(Motor1B,GPIO.LOW)
-	GPIO.output(Motor1Enable,GPIO.LOW)
-	GPIO.output(Motor2A,GPIO.LOW)
-	GPIO.output(Motor2B,GPIO.LOW)
-	GPIO.output(Motor2Enable,GPIO.LOW)
+def forwardGPIO():
+    pwm1.start(0)
+    pwm1.ChangeDutyCycle(100)
+    pwm2.start(0)
+    pwm2.ChangeDutyCycle(100)
+    GPIO.output(03,True)
+    GPIO.output(05,False)
+    GPIO.output(07,True)
+    GPIO.output(11,False)
+    sleep(0.8)
+    pwm1.stop()
+    pwm2.stop()
+    
+def reverseGPIO():
+    pwm1.start(0)
+    pwm1.ChangeDutyCycle(100)
+    pwm2.start(0)
+    pwm2.ChangeDutyCycle(100)
+    GPIO.output(03,False)
+    GPIO.output(05,True)
+    GPIO.output(07,False)
+    GPIO.output(11,True)
+    sleep(0.8)
+    pwm1.stop()
+    pwm2.stop()
+    
+def rightGPIO():
+    pwm1.start(0)
+    pwm1.ChangeDutyCycle(60)
+    GPIO.output(03,False)
+    GPIO.output(05,True)
+    sleep(0.8)
+    pwm1.stop()
+    
+def leftGPIO():
+    pwm2.start(0)
+    pwm2.ChangeDutyCycle(60)
+    GPIO.output(07,True)
+    GPIO.output(11,False)
+    sleep(0.8)
+    pwm2.stop()
+    
+def stopGPIO():
+    pwm1.stop()
+    pwm2.stop()
+    
+def clean():
+    GPIO.cleanup()
